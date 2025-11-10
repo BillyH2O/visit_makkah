3c0f784057a51a7762056a134b47aafb02c11235
BillyH2O <billy93.h2o@gmail.com>
2025-11-10 16:23:30 +0100

    🎨

--- components/ui/hero/HeroSection.tsx
+++ components/ui/hero/HeroSection.tsx
@@ -21,7 +21,7 @@ const HeroSection = ({
       style={{ backgroundImage: `url('${backgroundImage}')` }}
     >
       <div className='w-[90%] md:w-full mx-auto min-h-screen flex flex-col items-center justify-center gap-16 bg-cover bg-center py-24 px-4 text-white z-20'>
-          <h1 className='w-full md:max-w-[800px] lg:max-w-[1000px] xl:max-w-[1500px] text-5xl sm:text-6xl md:text-7xl 2xl:text-8xl font-bold text-center'>{title}</h1>
+          <h1 className='w-full md:max-w-[800px] lg:max-w-[1000px] xl:max-w-[1500px] text-3xl sm:text-4xl md:text-5xl 2xl:text-7xl font-bold text-center'>{title}</h1>
           <p className='w-full max-w-6xl md:w-full text-lg font-medium md:text-2xl text-center'>{description}</p>
           <div className='flex flex-col sm:flex-row items-center justify-center gap-4'>
               <Button label={button1Label} size='sm' variant='primary' href='/formules' blur={true} />
