dbf1e46c34b94353f98033c11b86517332f66d29
BillyH2O <billy93.h2o@gmail.com>
2025-11-10 15:37:35 +0100

    🐛

--- components/features/landing/offer/OfferCard.tsx
+++ components/features/landing/offer/OfferCard.tsx
@@ -43,7 +43,7 @@ const OfferCard = ({
 
   return (
     <Link
-      href={href}
+      href={baseHref}
       className={cn(
         "relative rounded-4xl overflow-hidden border-2 border-primary group cursor-pointer transition-all duration-300 hover:scale-[1.02] hover:shadow-xl hover:shadow-primary/20",
         sizeClassName ?? "w-full h-[300px] md:h-[300px] lg:h-[400px] xl:h-[450px]",
