3ef2fcbc2ee4317f6e1603b8cd56f1959cafdc6a
BillyH2O <billy93.h2o@gmail.com>
2025-11-10 15:43:17 +0100

    🐛

--- components/ui/hero/Cloud.tsx
+++ components/ui/hero/Cloud.tsx
@@ -2,9 +2,9 @@ import Image from 'next/image'
 
 const Cloud = () => {
   return (
-    <div className="relative w-full mb-32 md:mb-0">
+    <div className="relative w-full mb-32 md:mb-0 z-10">
         <Image src="/images/nuage2.png" alt="Makkah" width={1920} height={1080}
-        className="w-full h-auto mt-[-90px] md:mt-[-200px] lg:mt-[-300px]"
+        className="w-full h-auto mt-[-90px] md:mt-[-200px] lg:mt-[-250px] xl:mt-[-300px] 2xl:mt-[-350px]"
         priority
         />
     <div className="bg-linear-to-b from-white/0 to-white w-full h-full absolute top-0 left-0 right-0 bottom-0" />
--- components/ui/hero/HeroSection.tsx
+++ components/ui/hero/HeroSection.tsx
@@ -20,7 +20,7 @@ const HeroSection = ({
       className="w-full bg-cover bg-center"
       style={{ backgroundImage: `url('${backgroundImage}')` }}
     >
-      <div className='w-[90%] md:w-full mx-auto min-h-screen flex flex-col items-center justify-center gap-16 bg-cover bg-center py-24 px-4 text-white'>
+      <div className='w-[90%] md:w-full mx-auto min-h-screen flex flex-col items-center justify-center gap-16 bg-cover bg-center py-24 px-4 text-white z-20'>
           <h1 className='w-full md:max-w-[800px] lg:max-w-[1000px] xl:max-w-[1500px] text-5xl sm:text-6xl md:text-7xl 2xl:text-8xl font-bold text-center'>{title}</h1>
           <p className='w-full max-w-6xl md:w-full text-lg font-medium md:text-2xl text-center'>{description}</p>
           <div className='flex flex-col sm:flex-row items-center justify-center gap-4'>
