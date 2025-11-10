bc2564d68129e3a973b2a4073a7f652b4dca00bf
BillyH2O <billy93.h2o@gmail.com>
2025-11-10 15:59:01 +0100

    🎨

--- components/ui/hero/Cloud.tsx
+++ components/ui/hero/Cloud.tsx
@@ -4,7 +4,7 @@ const Cloud = () => {
   return (
     <div className="relative w-full mb-32 md:mb-0 z-10">
         <Image src="/images/nuage2.png" alt="Makkah" width={1920} height={1080}
-        className="w-full h-auto mt-[-90px] md:mt-[-150px] lg:mt-[-300px] xl:mt-[-320px] 2xl:mt-[-350px]"
+        className="w-full h-auto mt-[-90px] sm:mt-[-100px] md:mt-[-150px] lg:mt-[-300px] xl:mt-[-320px] 2xl:mt-[-350px]"
         priority
         />
     <div className="bg-linear-to-b from-white/0 to-white w-full h-full absolute top-0 left-0 right-0 bottom-0" />
